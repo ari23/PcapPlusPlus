962920e4c802faa649b6c97bb540a89d43cad3bb
seladb <pcapplusplus@gmail.com>
2020-07-12 16:54:03 -0700

    bla11

--- Pcap++/src/NetworkUtils.cpp
+++ Pcap++/src/NetworkUtils.cpp
@@ -190,7 +190,7 @@ MacAddress NetworkUtils::getMacAddress(IPv4Address ipAddr, PcapLiveDevice* devic
 	// check if timeout expired
 	if (res == ETIMEDOUT)
 	{
-		LOG_ERROR("ARP request time out\n");
+		LOG_ERROR("ARP request time out");
 		return result;
 	}
 
